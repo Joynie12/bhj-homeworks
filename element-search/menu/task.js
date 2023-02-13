b965eabd38442fdec78ce5192c6cdc340c749833
Joynie12 <bigw199412@gmail.com>
2023-02-13 09:15:09 +0300

    Выполнено

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,19 +1,19 @@
-// const close = document.querySelectorAll("a.menu__link");
-
 const sub = document.querySelectorAll("ul.menu_sub");
 const menuAll = Array.from(document.querySelectorAll("li.menu__item"));
-const stopLink = document.querySelectorAll("a.menu__link");
-
-stopLink.addEventListener("click", function(event) {
-  event.preventDefault();
-}, false);
 
 menuAll.forEach(element => {
-    element.querySelector("a").onclick = () => {
-        sub.forEach(elementUl => elementUl.className = "menu menu_sub");
-        element.querySelector("ul").className = "menu menu_sub menu_active";
-        return false;
+    if (element.closest(".menu_sub") === null) {
+        element.querySelector("a").onclick = () => {
+            if (element.querySelector("ul") && element.querySelector("ul").className === "menu menu_sub menu_active") {
+                element.querySelector("ul").className = "menu menu_sub";
+                return false;
+            } else {
+            sub.forEach(elementUl => elementUl.className = "menu menu_sub");
+                if (element.querySelector("ul")) {
+                    element.querySelector("ul").className = "menu menu_sub menu_active";
+                    return false;
+                }
+           }   
+        }
     }
 });
-
-
